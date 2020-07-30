6ff273a9553b5fbe5005b6d5516bf2da3b06383b
partyladd7 <partyladd7@gmail.com>
2020-07-30 20:02:42 +0300

    Переробив

--- AquariumGame/Controllers/TestC.cs
+++ AquariumGame/Controllers/TestC.cs
@@ -18,51 +18,83 @@ namespace AquariumGame.Controllers
             work.AddFish();
             gun = new Gun();
         }
+
+        public bool Perevirka(MediumFish a)
+        {
+            if (a.IsSatisfied==1)
+            {
+                return true;
+            }
+            return false;
+
+
+
+        }
+
+
         public void GunSetorGetFish(int StackID ) 
         {
-            if (gun.Get() == null)// потрібен метод який повертає чи є рибка у gun і яка вона
+            if (gun.Content() == null)// потрібен метод який повертає чи є рибка у gun і яка вона
             {
-                //gun.Set(work.GetStak(StackID));// потрібно передати id бо нема доступу до стеків і в класі work добавити метод що повертає стек по id  
+                gun.Set(work.GetStack(StackID));// потрібно передати id бо нема доступу до стеків і в класі work добавити метод що повертає стек по id  
                 //на view добавити обработчик що буде вертати номер колонки
             }
             else
             {
-                //Stack<Fish> f =  work.GetStak(StackID);
-                //if (f.Peek().GetType().Name == "BigFish" && gun.Get().GetType().Name == "MediumFish")// потрібен метод який повертає чи є рибка у gun і яка вона
-                //{
-                //    BigFish b = (BigFish)f.Peek();
-                //    if (b.IsSatisfied == 1)
-                //    {
-                //        f.Pop();// видаляю рибу
-                //    }
-                //    else 
-                //    {
-                //        b.Eat(gun.Get());
-                //    }
-
-                //}
-                //else if (f.Peek().GetType().Name == "MediumFish" && gun.Get().GetType().Name == "SmallFish")
-                //{
-                //    MediumFish b = (MediumFish)f.Peek();
+                Stack<Fish> f = work.GetStack(StackID);
+               
+               
+                if (f.Peek().GetType() == new BigFish().GetType() && gun.Content().GetType() == new MediumFish().GetType())// потрібен метод який повертає чи є рибка у gun і яка вона
+                {
+                    BigFish b = (BigFish)f.Peek();
                     
-                //    if (b.IsSatisfied == 1)
-                //    {
-                //        f.Pop();// видаляю рибу
-                //    }
-                //    else
-                //    {
-                //        b.Eat((SmallFish)gun.Get());
-                //    }
-                //}
+                    if (Perevirka((MediumFish)gun.Content()))
+                    {
+                        f.Pop();// видаляю рибу
+                        gun.Get();
+                    }
+                    else if (b.IsSatisfied == 1)
+                    {
+                        f.Pop();// видаляю рибу
+                        gun.Get();
+                    }
+                    else
+                    {
+                        b.Eat((MediumFish)gun.Get());
+                    }
+
+                }
+                else if (f.Peek().GetType() == new MediumFish().GetType() && gun.Content().GetType() == new SmallFish().GetType())
+                {
+                    MediumFish b = (MediumFish)f.Peek();
+
+                    if (b.IsSatisfied == 1)
+                    {
+                        f.Pop();// видаляю рибу
+                        gun.Get();
+                    }
+                    else
+                    {
+                        b.Eat((SmallFish)gun.Get());
+                    }
+                }
             }
 
 
 
         }
+        public void Refresh() // перестворити
+        {
+            Start();
+        }
+
         public void Fun()
         {
-
+            
         }
+
+
+
 
     }
 }
